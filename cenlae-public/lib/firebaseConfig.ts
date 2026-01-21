2d05a01033ed53997f6a1359c44d4fdd781bf75c
nicjespinoza <nicjespinoza@gmail.com>
2026-01-20 20:06:31 -0600

    feat: setup multi-site architecture with nextjs public site

---
+++ cenlae-public/lib/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD4DCL5CbT69g-ITeIj4Mpe0Q13XzxJZ28",
+    authDomain: "medirecord-pro-c84d0.firebaseapp.com",
+    projectId: "medirecord-pro-c84d0",
+    storageBucket: "medirecord-pro-c84d0.firebasestorage.app",
+    messagingSenderId: "625640839968",
+    appId: "1:625640839968:web:b9a35ab7a4f1d3fe089545",
+    measurementId: "G-ZXBDQ8YXKF"
+};
